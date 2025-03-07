f79557ff4fb8e34cd58ee208fc1e606c9f9b2bf5
Quinton Robbins <q@qntnrbns.com>
2025-03-06 23:21:17 -0500

    making the build work

--- valis/models/authentication_response.py
+++ valis/models/authentication_response.py
@@ -18,7 +18,7 @@ import re  # noqa: F401
 import json
 
 from pydantic import BaseModel, ConfigDict, Field, StrictBool, StrictInt, StrictStr
-from typing import Any, ClassVar, Dict, List, Optional
+from typing import Any, ClassVar, Dict, Optional
 from valis.models.group_roles_response import GroupRolesResponse
 from typing import Optional, Set
 from typing_extensions import Self
--- valis/models/create_partner_request.py
+++ valis/models/create_partner_request.py
@@ -19,7 +19,7 @@ import json
 
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field, StrictStr
-from typing import Any, ClassVar, Dict, List, Optional
+from typing import Any, ClassVar, Dict, Optional
 from typing_extensions import Annotated
 from typing import Optional, Set
 from typing_extensions import Self
